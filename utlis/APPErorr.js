a064b249fc2aaabefbbfed08e1816eb5baf2c505
bedoshakdofa <abdullahshakdofa@gmail.com>
2023-10-30 15:43:41 +0200

    natours

--- utlis/APPErorr.js
+++ utlis/APPErorr.js
@@ -3,6 +3,7 @@ class appErorr extends Error {
     super(message);
     this.statusCode = statusCode;
     this.status = `${this.statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isoperational = true;
     Error.captureStackTrace(this, this.constructor);
   }
 }
